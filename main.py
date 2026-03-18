52411c5f942f88a5fd25066f61bbcae3c9d82da6
Kororrro <korofm40@gmail.com>
2026-03-18 09:44:57 +0100

    basics

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("This is a .apk scraper")
+import scrapers
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from bs4 import BeautifulSoup
+
+driver = scrapers.initialize(["no-sandbox","disable-dev-shm-usage"])
+driver.get("https://nmap.org")
+parags = driver.find_elements(By.TAG_NAME, "li")
+for i in range(len(parags)):
+    print(parags[i].text)
+
+time.sleep(1)
+scrapers.findAndClick(driver, "TAG_NAME", "a")
